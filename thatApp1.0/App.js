237c1571eb2c31139b3961835b213f820679993c
skshm2000 <sakshamselwal2000@gmail.com>
2024-11-09 18:29:19 +0530

    thatApp1.0

---
+++ thatApp1.0/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is text component</Text>
+      <Text style={styles.text}>NExt is to learn react native navigation</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+  }
+});
